b913ecd7d1dbe133109826c4bdda0e4360b58d33
Mike Westaway <miwestaw@microsoft.com>
2018-05-20 13:13:59 +0100

    tested deployment as per spec

---
+++ deploy2.sh
@@ -0,0 +1,1 @@
+az group deployment create --verbose --template-file template.json --parameters @parameters.json  adminUsername=miwestaw adminPassword=M4cclesfield. existingVirtualNetworkResourceGroupName=gsk-vmss-poc existingVirtualNetworkName=myvnet existingSubnetName=default loadBalancerPrivateIpAddress=10.4.0.8 --resource-group 'gsk-bespoke-vmss-lb'  
